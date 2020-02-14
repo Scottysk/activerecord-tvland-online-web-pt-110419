494b0c5afd6d4f483bd69ad2fd82b79fd934eaaf
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-02-14 04:12:48 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,4 +1,4 @@
-class CreateCharacters < ActiveRecord::Migration
+class CreateCharacters < ActiveRecord::Migration[5.1]
     create_table :characters do |t|
       t.string :catchphrase
       t.string :name
